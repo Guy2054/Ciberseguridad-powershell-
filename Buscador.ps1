ad0a183347a13fefbfaf9d1b0ad05cd74a5e4e49
Guy2054 <simon.robledos@uanl.edu.mx>
2025-08-16 12:52:15 -0600

    Add files via upload
    Evidencias de la actividad 3 y 4

---
+++ Buscador.ps1
@@ -0,0 +1,22 @@
+﻿function Validar-Archivo {
+    param ([string]$Ruta)
+    
+    try {
+        if (Test-Path $Ruta) {
+            $contenido = Get-Content $Ruta -ErrorAction Stop
+            return "Archivo encontrado y accecible: $Ruta"
+        } else {
+            throw "El archivo no existe"
+        }
+    }
+    catch {
+        return "Error: $_"
+    }
+    finally {
+        Write-Host "Validación finalizada para: $Ruta" -ForegroundColor Cyan
+    }
+}
+
+Validar-Archivo -Ruta "C:\archivo_inexistente"
+
+Validar-Archivo -Ruta "$env:USERPROFILE\Desktop\archivo.txt"
---
+++ auditoria_usuarios.ps1
@@ -0,0 +1,35 @@
+﻿$usuarios = Get-LocalUser
+$sinLogon = @()
+$conLogon = @()
+foreach ($u in $usuarios){
+    if (-not $u.LastLogon) {
+        $sinLogon += "$($u.Name): Estado = $($u.Enabled), Último acceso = NUNCA"
+    } else{
+        $conLogon += "$($u.Name): Estado = $($u.Enabled), Último acceso = $($u.LastLogon)"
+    }
+}
+
+Set-Content -Path "C:\Users\simon\Desktop\reporte_usuarios.txt" -Value "Usuarios que no han iniciado sesión"
+ForEach ($usuario in $sinLogon) {
+    Add-Content -Path "C:\Users\simon\Desktop\reporte_usuarios.txt" -Value $usuario
+}
+Add-Content -Path "C:\Users\simon\Desktop\reporte_usuarios.txt" -Value " "
+Add-Content -Path "C:\Users\simon\Desktop\reporte_usuarios.txt" -Value "Usuarios que han iniciado sesión"
+ForEach ($usuario in $conLogon) {
+    Add-Content -Path "C:\Users\simon\Desktop\reporte_usuarios.txt" -Value $conLogon
+}
+
+if ($sinLogon -gt 0) {
+    Write-Output "`n Usuarios que NUNCA han iniciado sesión"
+    $sinLogon | ForEach-Object {Write-Output $_}
+    if ($conLogon -gt 0) {
+        Write-Output "`n Usuarios que SÍ han iniciado sesión"
+        $conLogon | ForEach-Object {Write-Output $_}
+    } else {
+        Write-Output "Ningun usuario ha iniciado sesión"
+    }
+} else {
+    Write-Output "Todos los usuarios han iniciado sesión"
+    $conLogon | ForEach-Object {Write-Output $_}
+}
+
